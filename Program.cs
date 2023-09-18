ca1f84aadc782031947427641439301c25f40639
UsmanSaddique <56198746+UsmanSaddique@users.noreply.github.com>
2023-09-18 11:47:41 +0500

    Add files via upload
    Program Description
    PDF Roll Number Scraper
    This C# console application is designed to scrape roll numbers from a PDF file containing academic records. It extracts roll numbers based on predefined criteria and processes them in batches. The program is particularly useful for educational institutions or researchers who need to extract roll numbers from large PDF documents.
    
    Features
    Extracts roll numbers from a specified PDF file.
    Filters and processes roll numbers based on user-defined criteria (e.g., specific pages, keyword-based filtering).
    Divides the roll numbers into batches for sequential or parallel processing.
    Saves images of the academic records in the "bin/debug" folder for easy access.
    Provides a flexible framework for adding custom processing logic.
    Displays progress and extracted roll numbers in the console.
    How to Use
    Clone or download this repository from GitHub.
    
    Build the project using a C# development environment (e.g., Visual Studio).
    
    Run the compiled executable file from the command line or terminal.
    
    Provide the path to the PDF file you want to process. You can specify the PDF file path in the pdfFilePath variable.
    
    The program will extract roll numbers from the PDF file, filter them based on criteria specified in the code (e.g., keyword filtering), and divide them into batches.
    
    The extracted roll numbers will be displayed in the console along with progress updates.
    
    Images of the academic records will be saved in the "bin/debug" folder of the project for easy access.
    
    You can customize the program by modifying the code to suit your specific requirements. For example, you can change the filtering criteria or add additional processing steps.
    
    Requirements
    .NET Framework or .NET Core runtime installed on your computer.
    PDF file containing academic records.
    Note
    This program serves as a starting point for PDF text extraction and processing. Depending on your specific use case, you may need to further customize the code to meet your exact needs.
    
    License
    This program is provided under the MIT License, allowing you to use and modify it freely.

---
+++ Program.cs
@@ -0,0 +1,138 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Drawing;
+
+string pdfFilePath = "file:///C:/Gazette%20A23.pdf";
+
+List<string> rollNumbers = new List<string>(); // List to store roll numbers
+
+using (PdfReader reader = new PdfReader(pdfFilePath))
+{
+    int record = 0;
+    for (int page = 36; page <= 1282; page++)
+    {
+        // Extract text from the current page
+        string pageText = PdfTextExtractor.GetTextFromPage(reader, page, new SimpleTextExtractionStrategy());
+
+        // Process the extracted text to extract Roll No and Student Name
+        ProcessPageText(pageText, ref record, rollNumbers);
+    }
+}
+
+// Display the list of roll numbers
+foreach (string rollNumber in rollNumbers)
+{
+    Console.WriteLine(rollNumber);
+    SaveImage(rollNumber);
+}
+
+
+Console.WriteLine("All tasks completed.");
+Console.ReadLine();
+    
+
+    static void ProcessPageText(string pageText, ref int record, List<string> rollNumbers)
+{
+    // Split the page text into lines
+    string[] lines = pageText.Split('\n');
+
+
+
+    // Process each line
+    foreach (var line in lines)
+    {
+        // Check if the line contains relevant information (Roll No and Student Name)
+        if (IsRelevantInformation(line))
+        {
+            // Extract and process Roll No and Student Name
+            ExtractAndProcessInfo(line, ref record, rollNumbers);
+        }
+    }
+}
+
+static bool IsRelevantInformation(string line)
+{
+    // Implement your logic to determine if the line contains relevant information
+    // For example, check if the line contains Roll No and Student Name
+    // You can use regular expressions or other string matching methods
+
+    // Example:
+    return line.ToLower().Contains("StudentName") || line.ToLower().Contains("StudentNameOtherNameMayBe");
+}
+
+static void ExtractAndProcessInfo(string line, ref int record, List<string> rollNumbers)
+{
+    // Extract Roll No and Student Name from the line
+    // Implement your logic to extract the required information
+
+    // Example:
+    string[] parts = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+    foreach (var item in parts)
+    {
+        if (IsIntegerWithLength5(item))
+        {
+            // Store the roll number in the list
+            rollNumbers.Add(item);
+            Console.WriteLine($"Record {record}, Roll No: {item}");
+        }
+    }
+
+    record++;
+}
+
+static bool IsIntegerWithLength5(string str)
+{
+    int result;
+    return int.TryParse(str, out result) && str.Length == 6;
+}
+static  void SaveImage(string rollNumber)
+{
+    IWebDriver driver = new ChromeDriver();
+
+    try
+    {
+        driver.Navigate().GoToUrl("http://result.biselahore.com/");
+
+        // Find the text field and enter text into it
+        IWebElement textField = driver.FindElement(By.XPath("//*[@id='txtFormNo']"));
+        textField.SendKeys(rollNumber);
+
+        // Find the radio button and click it
+        IWebElement radioInput = driver.FindElement(By.XPath("//*[@id='rdlistCourse_1']"));
+        IJavaScriptExecutor executor = (IJavaScriptExecutor)driver;
+        executor.ExecuteScript("arguments[0].click();", radioInput);
+
+        // Find the button and click it
+        IWebElement button = driver.FindElement(By.XPath("//*[@id='Button1']"));
+        button.Click();
+
+        // Wait for a few seconds (adjust as needed) to ensure the PDF loads
+        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        IWebElement imageElement = wait.Until(driver =>
+        {
+            IWebElement element = driver.FindElement(By.XPath("//*[@id='imgDisplay']"));
+            return element.Displayed ? element : null;
+        });
+
+        // Get the location and size of the image element
+        Point location = imageElement.Location;
+        Size size = imageElement.Size;
+
+        // Take a screenshot of the entire page
+        Screenshot screenshot = ((ITakesScreenshot)imageElement).GetScreenshot();
+
+        // Convert the screenshot to a .NET Bitmap
+        screenshot.SaveAsFile($"{rollNumber}.png", ScreenshotImageFormat.Png);
+
+        // You can continue with the rest of your automation steps, such as waiting for the PDF download and saving it.
+    }
+    finally
+    {
+        // Don't forget to close the WebDriver when you're done
+        driver.Quit();
+    }
+}
